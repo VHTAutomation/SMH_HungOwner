3ad3da522c4fd0177b18e06889eda87424c9243e
Hung <Hung@DESKTOP-L55PO0H>
2024-12-23 01:17:16 +0700

    commit

---
+++ Scripts/QuanLyGuongMat/SuaGuongMat/QLGM28_cap nhat guong mat khi dong popup sua doi khong duoc luu/Script1734890687260.groovy
@@ -0,0 +1,48 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication(GlobalVariable.appID, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/title_thiet_bi'), 0)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManThietBi/opt_thiet_bi'), 0)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManThietBi/ObjectManChiTietThietBi/title_tinh_nang'), 0)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManThietBi/ObjectManChiTietThietBi/title_nhan_dien_guong_mat'), 0)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManThietBi/ObjectManChiTietThietBi/btn_quan_ly_guong_mat'), 0)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManCaNhan/ObjectManQuanLyChung/ObjectManQuanLyGuongMat/opt_guong_mat'), 
+    0)
+
+Mobile.clearText(findTestObject('ObjectManHinhChinh/ObjectManCaNhan/ObjectManQuanLyChung/ObjectManQuanLyGuongMat/txt_ten_guong_mat'), 
+    0)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManCaNhan/ObjectManQuanLyChung/ObjectManQuanLyGuongMat/btn_quay_lai_man_quan_ly_guong_mat'), 
+    0)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManCaNhan/ObjectManQuanLyChung/ObjectManQuanLyGuongMat/btn_huy_thong_bao_sua_doi_khong_duoc_luu'), 
+    0, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManCaNhan/ObjectManQuanLyChung/ObjectManQuanLyGuongMat/opt_anh_guong_mat'), 
+    0, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.closeApplication()
+

71e3cea5267dd92020cc4fad8d0bfd625e26228b
tester <tester.vht@gmail.com>
2024-12-26 17:36:13 +0700

    commit

---
+++ Scripts/QuanLyNha/ThemNha/QLN2_Them nha thanh cong tu tab trang chu - Copy/Script1735205570114.groovy
@@ -0,0 +1,85 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.testobject.ConditionType as ConditionType
+
+Mobile.startExistingApplication(GlobalVariable.appID, FailureHandling.STOP_ON_FAILURE)
+
+tapDynamicObject('//android.widget.TextView[1]', 0)
+
+Mobile.tap(findTestObject('ObjectThemNha/popup_chon _nha_Quan_Ly_Nha'), 0)
+
+//Mobile.tap(findTestObject('ObjectThemNha/btn_them_nha_tren_man_quan_ly_nha'), 0)
+//
+//Mobile.setText(findTestObject('ObjectThemNha/txtbox_nhap_ten_nha'), 'auto_trang_chu', 0)
+//
+//Mobile.tap(findTestObject('ObjectThemNha/txtbox_vi_tri_nha'), 0)
+//
+//Mobile.tap(findTestObject('ObjectThemNha/btn_Luu_tren_man_chon_dia_diem'), 0)
+//
+//Mobile.tap(findTestObject('ObjectThemNha/btn_Luu_tren_man_them_nha'), 0)
+//
+//Mobile.verifyElementVisible(findTestObject('ObjectThemNha/popup_them_nha_thanh_cong_image'), 0)
+//
+//Mobile.verifyElementVisible(findTestObject('ObjectThemNha/popup_them_nha_thanh_cong_title'), 0)
+//
+//Mobile.verifyElementVisible(findTestObject('ObjectThemNha/popup_them_nha_thanh_cong_noi_dung'), 0)
+//
+//Mobile.tap(findTestObject('ObjectThemNha/popup_them_nha_thanh_cong_btn_OK'), 0)
+//
+//TestObject houseItem = new TestObject('houseItem')
+//
+//houseItem.addProperty('xpath', ConditionType.EQUALS, "//android.widget.TextView[contains(@text, '$GlobalVariable.housename_trangchu')]")
+//
+//'Kiểm tra nhà vừa tạo trên màn Quản lý nhà'
+//Mobile.verifyElementExist(houseItem, 10 // timeout 10s
+//    )
+//
+//Mobile.tap(findTestObject('ObjectThemNha/icon_back'), 0)
+//
+//tapDynamicObject('//android.widget.TextView[1]', 0)
+//
+//'Kiểm tra nhà vừa tạo trên popup chọn nhà trên trang chủ'
+//Mobile.verifyElementExist(houseItem, 10)
+//
+//Mobile.tap(houseItem, 0)
+//
+//Mobile.tap(findTestObject('ObjectThemNha/Tab_Thiet_Bi'), 0)
+//
+//'Kiểm tra nhà vừa tạo trên popup chọn nhà trên tab thiết bị'
+//Mobile.verifyElementExist(houseItem, 10)
+
+//Mobile.verifyElementExist(findTestObject('null'), 0)
+// Tạo Test Object động
+//Tạo một Test Object với tên dynamicObject.Test Object này chỉ tồn tại trong bộ nhớ trong khi script chạy và không được lưu trong Object Repository.
+// Sử dụng Khi không muốn lưu trữ quá nhiều Test Object trong Object Repository
+//Khi phần tử có vị trí, tên, hoặc thuộc tính thay đổi theo ngữ cảnh.
+// Khi cần kiểm thử động và linh hoạt với các điều kiện khác nhau
+//XPath của phần tử đầu tiên cần thao tác.
+// Thực hiện tap khi phần tử đã xuất hiện
+//Hành động "tap" (nhấn) lên phần tử được đại diện bởi dynamicObject.Thời gian chờ (timeout) là 0 giây.
+Mobile.closeApplication()
+
+def tapDynamicObject(String xpath, int timeout) {
+    TestObject dynamicObject = new TestObject('dynamicObject')
+
+    dynamicObject.addProperty('xpath', ConditionType.EQUALS, xpath)
+
+    Mobile.tap(dynamicObject, 0)
+}
+
---
+++ Test Suites/Happy Suite/New Test Suite.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2a9e2087-abfb-43c9-b8a9-2e95a20d5d8b</testSuiteGuid>
+   <testCaseLink>
+      <guid>9b8ef074-f5d8-4a88-8cca-1eec4f48dc7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/QuanLyTaiKhoan/DangNhap/QLTK1_dang nhap thanh cong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>135c49b7-7e87-4669-89e6-4d05b4616e8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/QuanLyTaiKhoan/DoiMatKhau/QLTK18_doi mat khau thanh cong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ecbe1e57-7b31-41a5-a2f8-493705271bb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/QuanLyNha/ThemNha/QLN2_Them nha thanh cong tu tab trang chu</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4da5c68f-be53-40f1-80c4-42713487b94f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/QuanLyNha/SuaNha/QLN8_Doi ten nha tu tab trang chu</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>16b45d42-7202-4938-aadf-a9bcdcd074d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/QuanLyNha/XoaNha/QLN14_Xoá nha tai tab trang chu</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
